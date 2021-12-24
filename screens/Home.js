08619d6f95483be336b45d38ad0f84de92616095
arsimmsejdiu <arsimmsejdiu@gmail.com>
2021-12-24 02:45:05 +0100

    added yelp API to the app

--- screens/Home.js
+++ screens/Home.js
@@ -1,15 +1,19 @@
 import React, { useState } from "react";
 import { View, Text, SafeAreaView, ScrollView } from "react-native";
-
 import Categories from "../components/Categories";
 import HeaderTabs from "../components/HeaderTabs";
 import RestaurantItems from "../components/RestaurantItems";
 import SearchBar from "../components/SearchBar";
-
 import { localRestaurants } from "../data/RestaurantData";
+
+const YelpApiKey = process.env.YELP_API_KEY;
 
 export default function Home() {
   const [restaurantData, setRestaurantData] = useState(localRestaurants);
+
+  const getRestaurantsFromYelp = () => {
+    const yelpUrl = `https://api.yelp.com/v3/businesses/search?term=restaurants&location=SanDiego`;
+  }
 
   return (
     <SafeAreaView style={{ backgroundColor: "#eee", flex: 1 }}>

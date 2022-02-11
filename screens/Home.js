b6dd538ad8568f3ccc7a57d9b92a3e2d4d48426b
arsimmsejdiu <arsimmsejdiu@gmail.com>
2022-02-11 01:28:01 +0100

    refactoring the code and file structure

--- screens/Home.js
+++ screens/Home.js
@@ -1,11 +1,11 @@
 import React, { useState, useEffect } from "react";
 import { View, Text, SafeAreaView, ScrollView } from "react-native";
 import { Divider } from "react-native-elements";
-import BottomTabs from "../components/BottomTabs";
-import Categories from "../components/Categories";
-import HeaderTabs from "../components/HeaderTabs";
-import RestaurantItems from "../components/RestaurantItems";
-import SearchBar from "../components/SearchBar";
+import BottomTabs from "../components/home/BottomTabs";
+import Categories from "../components/home/Categories";
+import HeaderTabs from "../components/home/HeaderTabs";
+import RestaurantItems from "../components/home/RestaurantItems";
+import SearchBar from "../components/home/SearchBar";
 import { localRestaurants } from "../data/RestaurantData";
 
 const YELP_API_KEY =
